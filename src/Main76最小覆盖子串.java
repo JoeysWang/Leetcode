11cac8a57931f6b6c6eca96aba2f42919be7f5e1
Yue.wang <yue.wang@atomefin.com.cn>
2020-06-11 22:31:54 +0800

    滑动窗口

--- src/Main76最小覆盖子串.java
+++ src/Main76最小覆盖子串.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Main76最小覆盖子串 {
     /**
      * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
@@ -22,8 +24,66 @@ public class Main76最小覆盖子串 {
 
     public static String minWindow(String s, String t) {
 
+        HashMap<Character, Integer> needs = new HashMap<>();
 
-        return "";
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        for (int i = 0; i < t.length(); i++) {
+            Integer integer = needs.getOrDefault(t.charAt(i), 0);
+            needs.put(t.charAt(i), integer + 1);
+        }
+
+        // 记录最小覆盖子串的起始索引及长度
+        int start = 0, len = Integer.MAX_VALUE;
+
+
+        int left = 0, right = 0;
+        //valid 变量表示窗口中满足 need 条件的字符个数
+        int valid = 0;
+
+        while (right < s.length()) {
+            // 开始滑动
+            // c 是将移入窗口的字符
+            char c = s.charAt(right);
+
+            right++;
+            // 进行窗口内数据的一系列更新
+            if (needs.containsKey(c)) {
+                //往窗口里加
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(needs.get(c))) {
+                    valid++;
+                }
+            }
+
+            //当valid计数等于 need的长度时，说明字符串数量已经匹配了
+            while (needs.size() == valid) {
+                // 在这里更新最小覆盖子串
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+
+                char d = s.charAt(left);
+                left++;
+                // 进行窗口内数据的一系列更新
+                if (needs.containsKey(d)) {
+                    if (needs.get(d).equals(window.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+
+                }
+
+
+            }
+
+        }
+
+
+        // 返回最小覆盖子串
+        return len == Integer.MAX_VALUE ?
+                "" : s.substring(start, start+len);
     }
 
 }
