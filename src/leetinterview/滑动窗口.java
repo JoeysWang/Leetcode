2ef55f148a43ab4a94d08ffc35aca2400ad262b5
Yue.wang <yue.wang@atomefin.com.cn>
2020-06-24 15:38:47 +0800

    补码 反码

--- src/leetinterview/滑动窗口.java
+++ src/leetinterview/滑动窗口.java
@@ -1,11 +1,34 @@
 package leetinterview;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class 滑动窗口 {
 
     public static void main(String[] args) {
+
+//        LinkedHashMap<Integer, Integer> a = new LinkedHashMap<>(10, 0.75f, true);
+//
+//        for (int i = 0; i < 10; i++) {
+//            a.put(i, i);
+//        }
+//        a.get(2);
+//        a.get(3);
+////
+//        Map.Entry<Integer, Integer> next = a.entrySet().iterator().next();
+//
+//
+//        System.out.println(a.toString());
+//        System.out.println(next.getKey() + "," + next.getValue());
+
+
+        int a = -18;
+        int b = ~a;
+//        int c = !a;
+        System.out.println("10进制 a =  " + a + "   2进制 a = " + Integer.toBinaryString(a));
+        System.out.println("10进制 b =  " + b + "   2进制 b = " + Integer.toBinaryString(b));
 
 
     }
@@ -15,7 +38,7 @@ public class 滑动窗口 {
 
         public int[] maxSlidingWindow(int[] nums, int k) {
 
-            int[] res = new int[nums.length-k+1];
+            int[] res = new int[nums.length - k + 1];
 
             PriorityQueue<Integer> queue = new PriorityQueue<>(k, new Comparator<Integer>() {
                 @Override
@@ -32,15 +55,15 @@ public class 滑动窗口 {
                 queue.offer(nums[i]);
             }
 
-            while (right < nums.length ) {
+            while (right < nums.length) {
                 res[left] = queue.peek();
 
                 int out = nums[left];
                 queue.remove(out);
                 left++;
                 right++;
-                if (right<nums.length)
-                queue.add(nums[right]);
+                if (right < nums.length)
+                    queue.add(nums[right]);
             }
             return res;
         }
